11bd3c32eeb13dc3f0d1cdd869189f39d2c7d08b
uitstudio <>
2013-10-21 20:41:33 +0800

    Altering the project model class

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -159,4 +159,24 @@ class Project extends TrackStarActiveRecord
 		$command->bindValue(":role", $role, PDO::PARAM_STR);
 		return $command->execute()==1 ? true : false;
 	}
+  
+  /**
+	 * Returns an array of available roles in which a user can be placed when being added to a project
+	 */
+	public static function getUserRoleOptions()
+	{
+		return CHtml::listData(Yii::app()->authManager->getRoles(), 'name', 'name');    
+	}
+  
+  /* 
+	 * Determines whether or not a user is already part of a project
+	 */
+	public function isUserInProject($user) 
+	{
+		$sql = "SELECT user_id FROM tbl_project_user_assignment WHERE project_id=:projectId AND user_id=:userId";
+		$command = Yii::app()->db->createCommand($sql);
+		$command->bindValue(":projectId", $this->id, PDO::PARAM_INT);
+		$command->bindValue(":userId", $user->id, PDO::PARAM_INT);
+		return $command->execute()==1;
+	}
 }
