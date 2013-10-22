ea636ffeb50ba9d36b10ded3c674d218d039c739
uitstudio <>
2013-10-22 13:30:53 +0800

    More on relational AR queries in Yii

--- protected/controllers/IssueController.php
+++ protected/controllers/IssueController.php
@@ -31,7 +31,7 @@ class IssueController extends Controller
 	 */
 	public function actionView($id)
 	{
-    $issue=$this->loadModel($id);
+    $issue=$this->loadModel($id,true);
 		$comment=$this->createComment($issue);
 		$this->render('view',array(
 			'model'=>$issue,
@@ -140,8 +140,11 @@ class IssueController extends Controller
 	 * @return Issue the loaded model
 	 * @throws CHttpException
 	 */
-	public function loadModel($id)
+	public function loadModel($id,$withComments=false)
 	{
+    if($withComments)
+      $model = Issue::model()->with(array('comments'=>array('with'=>'author')))->findByPk($id);
+    else
 		$model=Issue::model()->findByPk($id);
 		if($model===null)
 			throw new CHttpException(404,'The requested page does not exist.');
